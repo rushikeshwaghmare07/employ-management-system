b4b136640616628429d4b7ebdff0b8284628be6b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-10-24 19:37:41 +0530

    feat: create task form for (admin-dashboard)

---
+++ src/components/Dashboard/Widgets/CreateTask.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const CreateTask = () => {
+  return (
+    <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
+      <form className="flex flex-wrap w-full items-start justify-between">
+        <div className="w-1/2 lg:w-1/2">
+          <div>
+            <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
+            <input
+              className="text-sm px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
+              type="text"
+              placeholder="Design a UI for Homepage"
+            />
+          </div>
+          <div>
+            <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
+            <input
+              className="text-sm px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 "
+              type="date"
+            />
+          </div>
+          <div>
+            <h3 className="text-sm text-gray-300 mb-0.5">Assign to</h3>
+            <input
+              className="text-sm px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 "
+              type="text"
+              placeholder="employee name"
+            />
+          </div>
+          <div>
+            <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
+            <input
+              className="text-sm px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 "
+              type="text"
+              placeholder="Design, Development, Marketing, etc"
+            />
+          </div>
+        </div>
+
+        <div className="w-1/2 lg:w-1/2">
+          <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
+          <textarea
+            className="w-full h-44 text-sm px-4 rounded outline-none bg-transparent border-[1px] border-gray-400"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className="bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full">
+            Create Task
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateTask;
