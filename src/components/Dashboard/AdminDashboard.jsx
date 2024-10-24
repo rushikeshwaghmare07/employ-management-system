b00665fc6f05c57d34ba9e6b6b17c6cc87db98e4
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-10-24 19:40:14 +0530

    update admin-dashboard

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Header from "../Layout/Header"
+import React from "react";
+import Header from "../Layout/Header";
+import CreateTask from "./Widgets/CreateTask";
+import AllTask from "./Widgets/AllTask";
 
 const AdminDashboard = () => {
   return (
     <div className="h-screen w-full p-10">
       <Header />
+      <CreateTask />
+      <AllTask />
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
