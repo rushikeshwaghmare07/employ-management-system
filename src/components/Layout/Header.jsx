119cd1b696ec03eada4c2473b2f83070d37639bf
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-08 17:34:20 +0530

    Update the styling of header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Header = ({data}) => {
 
   return (
-    <div className='flex flex-col sm:flex-row items-center sm:items-end justify-between font-medium'>
+    <div className='flex flex-col sm:flex-row items-center sm:items-end justify-between font-medium mt-6 sm:smt-0'>
       <h1 className='text-2xl sm:text-left text-center'>
         Hello, <br /> 
         <span className='text-3xl sm:text-4xl font-semibold'>{data.firstName} 👋</span> 
       </h1>
-      <button className='bg-red-500 text-white text-lg font-medium px-5 py-2 rounded-sm mt-4 sm:mt-0'>
+      <button className='bg-red-500 text-white text-lg font-medium px-5 py-2 rounded-md mt-4 sm:mt-0 hover:bg-red-700 transition'>
         Log Out
       </button>
     </div>
