469f9f56374df9360f0fc91978c9a70bff71ae62
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-08 16:29:59 +0530

    Update Employee dashboard and send data to other component

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,35 @@ import React, { useContext, useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
-import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
+import { employees, getLocalStorage, setLocalStorage } from "./utils/localStorage";
 import { AuthContext } from "./context/AuthProvider";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loggedInUserData, setLoggedInUserData] = useState(null);
   const authData = useContext(AuthContext)
   // console.log(authData.employeesData)
 
-  useEffect(() => {
-    if (authData) {
-      const loggedInUser = localStorage.getItem("loggedInUser");
-      if (loggedInUser) {
-        setUser(loggedInUser.role);
-      }
-    }
-  }, [authData])
+  // useEffect(() => {
+  //   if (authData) {
+  //     const loggedInUser = localStorage.getItem("loggedInUser");
+  //     if (loggedInUser) {
+  //       setUser(loggedInUser.role);
+  //     }
+  //   }
+  // }, [authData])
 
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == "123") {
       setUser("admin");
       localStorage.setItem("loggedInUser", JSON.stringify({role: "admin"}));
-    } else if (authData && authData.employeesData.find((e) => email == e.email && e.password == password)) {
-      setUser("employee");
-      localStorage.setItem("loggedInUser", JSON.stringify({role: "employee"}));
+    } else if (authData) {
+      const employee = authData.employeesData.find((e) => email == e.email && e.password == password);
+      if (employee) {
+        setUser("employee");
+        setLoggedInUserData(employee);
+        localStorage.setItem("loggedInUser", JSON.stringify({role: "employee"}));
+      }
     } else {
       alert("Invalid credential");
     }
@@ -34,7 +39,7 @@ function App() {
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ""}
-      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard /> }
+      {user == "admin" ? <AdminDashboard /> : (user == "employee" ? <EmployeeDashboard data={loggedInUserData} /> : null)  }
     </>
   );
 }
--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,12 +3,12 @@ import Header from '../Layout/Header';
 import TaskSummary from './Widgets/TaskSummary';
 import TaskList from '../TaskList/TaskList';
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = ({data}) => {
   return (
     <div className='p-5 sm:p-10 bg-[#1C1C1C] min-h-screen'>
-      <Header />
-      <TaskSummary />
-      <TaskList />
+      <Header data={data} />
+      <TaskSummary data={data} />
+      <TaskList data={data} />
     </div>
   );
 };
