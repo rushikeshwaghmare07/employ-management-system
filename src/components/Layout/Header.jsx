edbf5846058e488ebc771665afe54f47b818376b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-08 16:31:28 +0530

    Update current user name

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Header = () => {
+const Header = ({data}) => {
+
   return (
     <div className='flex flex-col sm:flex-row items-center sm:items-end justify-between font-medium'>
       <h1 className='text-2xl sm:text-left text-center'>
         Hello, <br /> 
-        <span className='text-3xl sm:text-4xl font-semibold'>User 👋</span> 
+        <span className='text-3xl sm:text-4xl font-semibold'>{data.firstName} 👋</span> 
       </h1>
       <button className='bg-red-500 text-white text-lg font-medium px-5 py-2 rounded-sm mt-4 sm:mt-0'>
         Log Out
