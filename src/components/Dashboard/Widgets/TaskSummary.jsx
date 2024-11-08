2d943e09020e3a262a4b51cd244b4fb96e32941c
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-08 17:34:54 +0530

    Update styling in task summary

--- src/components/Dashboard/Widgets/TaskSummary.jsx
+++ src/components/Dashboard/Widgets/TaskSummary.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 
 const TaskSummary = ({data}) => {
   return (
-    <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
-      <div className='rounded-xl py-6 px-9 bg-blue-500 text-white'>
+    <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
+      <div className='rounded-lg p-6 bg-blue-600 text-white shadow-md'>
         <h2 className='text-3xl font-semibold'>{data.taskCount.newTask}</h2>
-        <h3 className='text-2xl font-medium'>New Task</h3>
+        <h3 className='text-xl font-medium'>New Task</h3>
       </div>
-      <div className='rounded-xl py-6 px-9 bg-green-500 text-white'>
+      <div className='rounded-lg p-6 bg-green-600 text-white shadow-md'>
         <h2 className='text-3xl font-semibold'>{data.taskCount.completed}</h2>
-        <h3 className='text-2xl font-medium'>Completed</h3>
+        <h3 className='text-xl font-medium'>Completed</h3>
       </div>
-      <div className='rounded-xl py-6 px-9 bg-yellow-500 text-white'>
+      <div className='rounded-lg p-6 bg-yellow-500 text-white shadow-md'>
         <h2 className='text-3xl font-semibold'>{data.taskCount.active}</h2>
-        <h3 className='text-2xl font-medium'>Accepted</h3>
+        <h3 className='text-xl font-medium'>Accepted</h3>
       </div>
-      <div className='rounded-xl py-6 px-9 bg-red-500 text-white'>
+      <div className='rounded-lg p-6 bg-red-600 text-white shadow-md'>
         <h2 className='text-3xl font-semibold'>{data.taskCount.failed}</h2>
-        <h3 className='text-2xl font-medium'>Failed</h3>
+        <h3 className='text-xl font-medium'>Failed</h3>
       </div>
     </div>
   );
