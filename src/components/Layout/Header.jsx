a67b873f82962e0a41981a7a7abfa3e7f5c5348c
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-08 18:06:40 +0530

    Add logout functionality

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { setLocalStorage } from '../../utils/localStorage';
 
-const Header = ({data}) => {
+const Header = () => {
+
+  const [username, setUsername] = useState("");
+
+  // if (!data) {
+  //   setUsername("Admin")
+  // } else {
+  //   setUsername(data.firstName);
+  // }
+
+  const logOutUser = () => {
+    localStorage.setItem("loggedInUser", "");
+    window.location.reload();
+  }
 
   return (
     <div className='flex flex-col sm:flex-row items-center sm:items-end justify-between font-medium mt-6 sm:smt-0'>
       <h1 className='text-2xl sm:text-left text-center'>
         Hello, <br /> 
-        <span className='text-3xl sm:text-4xl font-semibold'>{data.firstName} 👋</span> 
+        <span className='text-3xl sm:text-4xl font-semibold'>username 👋</span> 
       </h1>
-      <button className='bg-red-500 text-white text-lg font-medium px-5 py-2 rounded-md mt-4 sm:mt-0 hover:bg-red-700 transition'>
+      <button onClick={logOutUser} className='bg-red-500 text-white text-lg font-medium px-5 py-2 rounded-md mt-4 sm:mt-0 hover:bg-red-700 transition'>
         Log Out
       </button>
     </div>
