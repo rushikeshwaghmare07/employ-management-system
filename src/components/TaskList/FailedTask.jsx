7ddc6c2a32f348f8d0bbc20e280e92cb29de2075
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-09 14:09:03 +0530

    Fix: Pass `elem` data to FailedTask in TaskList for failed tasks

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-
-const FailedTask = () => {
+const FailedTask = ({ data }) => {
   return (
     <div className="flex-shrink-0 w-72 p-5 bg-yellow-500 text-white rounded-lg shadow-lg">
       <div className="flex justify-between items-center">
-        <h3 className="bg-red-700 text-xs px-2 py-1 rounded">High</h3>
-        <h4 className="text-sm">20 Feb 2024</h4>
+        <h3 className="bg-red-700 text-xs px-2 py-1 rounded">
+          {data.category}
+        </h3>
+        <h4 className="text-sm">{data.date}</h4>
       </div>
-      <h2 className="mt-4 text-xl font-semibold">Make a YouTube Video</h2>
-      <p className="text-sm mt-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, tempora.
-      </p>
+      <h2 className="mt-4 text-xl font-semibold">{data.title}</h2>
+      <p className="text-sm mt-2">{data.description}</p>
       <div className="mt-4">
         <button className="w-full bg-white text-yellow-600 py-2 rounded-md hover:bg-yellow-100 transition">
           Failed
