f428c61e04e217920465456b4a180a28ecc5a6a4
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-09 14:08:45 +0530

    Fix: Pass `elem` data to NewTask in TaskList for new tasks

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-const NewTask = () => {
+const NewTask = ({ data }) => {
   return (
     <div className="flex-shrink-0 w-72 p-5 bg-green-500 text-white rounded-lg shadow-lg">
       <div className="flex justify-between items-center">
-        <h3 className="bg-red-700 text-xs px-2 py-1 rounded">High</h3>
-        <h4 className="text-sm">20 Feb 2024</h4>
+        <h3 className="bg-red-700 text-xs px-2 py-1 rounded">
+          {data.category}
+        </h3>
+        <h4 className="text-sm">{data.date}</h4>
       </div>
-      <h2 className="mt-4 text-xl font-semibold">Make a YouTube Video</h2>
-      <p className="text-sm mt-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, tempora.
-      </p>
+      <h2 className="mt-4 text-xl font-semibold">{data.title}</h2>
+      <p className="text-sm mt-2">{data.description}</p>
       <div className="mt-4">
         <button className="bg-white text-green-500 px-4 py-2 rounded-md hover:bg-green-100 transition w-full">
-          Accept Task
+          Accept data
         </button>
       </div>
     </div>
