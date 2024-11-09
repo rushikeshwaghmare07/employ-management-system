267f3ac96d33c37be08ac5014b5296db51b47bb3
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-09 14:07:52 +0530

    Fix: Pass `elem` data to AcceptTask in TaskList for active tasks

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const AcceptTask = () => {
+const AcceptTask = ({ data }) => {
   return (
     <div className="flex-shrink-0 w-72 p-5 bg-red-500 text-white rounded-lg shadow-lg">
       <div className="flex justify-between items-center">
-        <h3 className="bg-red-700 text-xs px-2 py-1 rounded">High</h3>
-        <h4 className="text-sm">20 Feb 2024</h4>
+        <h3 className="bg-red-700 text-xs px-2 py-1 rounded">
+          {data.category}
+        </h3>
+        <h4 className="text-sm">{data.date}</h4>
       </div>
-      <h2 className="mt-4 text-xl font-semibold">Make a YouTube Video</h2>
-      <p className="text-sm mt-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, tempora.
-      </p>
+      <h2 className="mt-4 text-xl font-semibold">{data.title}</h2>
+      <p className="text-sm mt-2">{data.description}</p>
       <div className="flex justify-between mt-4 space-x-2">
         <button className="bg-green-600 px-4 py-2 text-xs rounded-md hover:bg-green-700 transition">
           Mark as Completed
