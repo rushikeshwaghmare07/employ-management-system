57ab46ef232f6bd7a8bc16492833ba3adc6cf5e4
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-09 14:07:22 +0530

    Fix: Ensure correct rendering of task components in TaskList based on task status

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,26 +1,24 @@
 import React from "react";
-// import TaskCard from './TaskCard';
 import AcceptTask from "./AcceptTask";
 import NewTask from "./NewTask";
 import CompleteTask from "./CompleteTask";
 import FailedTask from "./FailedTask";
 
 const TaskList = ({ data }) => {
-  console.log(data);
   return (
     <div id="taskList" className="mt-10 flex overflow-x-auto space-x-6 pb-6">
       {data.tasks.map((elem, index) => {
         if (elem.active) {
-          return <AcceptTask key={index} />;
+          return <AcceptTask key={index} data={elem} />;
         }
         if (elem.newTask) {
-          <NewTask key={index} />;
+          <NewTask key={index} data={elem} />;
         }
         if (elem.completed) {
-          <CompleteTask key={index} />;
+          <CompleteTask key={index} data={elem} />;
         }
         if (elem.failed) {
-          <FailedTask key={index} />;
+          <FailedTask key={index} data={elem} />;
         }
         return null; // To handle any cases that don't match the above conditions
       })}
