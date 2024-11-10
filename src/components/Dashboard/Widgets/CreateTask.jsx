c0f88e101ab4dd741daf7b906ccd20829f63d33a
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-10 16:06:25 +0530

    Update CreateTask component with form inputs and localStorage integration

--- src/components/Dashboard/Widgets/CreateTask.jsx
+++ src/components/Dashboard/Widgets/CreateTask.jsx
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateTask = () => {
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDate, setTaskDate] = useState("");
+  const [assignTo, setAssignTo] = useState("");
+  const [category, setCategory] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+
+  const [newTask, setNewTask] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setNewTask({
+      taskTitle,
+      taskDate,
+      category,
+      taskDescription,
+      active: false,
+      newTask: true,
+      failed: false,
+      completed: false,
+    });
+
+    const data = JSON.parse(localStorage.getItem("employees"));
+
+    data.forEach((elem) => {
+      if (assignTo === elem.firstName) {
+        elem.tasks.push(newTask);
+      }
+    });
+
+    setTaskTitle("");
+    setTaskDate("");
+    setAssignTo("");
+    setCategory("");
+    setTaskDescription("");
+  };
+
   return (
     <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
-      <form className="flex flex-wrap w-full items-start justify-between">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="flex flex-wrap w-full items-start justify-between"
+      >
         <div className="w-1/2 lg:w-1/2">
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
             <input
               className="text-sm px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
+              value={taskTitle}
+              onChange={(e) => {
+                setTaskTitle(e.target.value);
+              }}
               type="text"
               placeholder="Design a UI for Homepage"
             />
@@ -18,12 +64,20 @@ const CreateTask = () => {
             <input
               className="text-sm px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 "
               type="date"
+              value={taskDate}
+              onChange={(e) => {
+                setTaskDate(e.target.value);
+              }}
             />
           </div>
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Assign to</h3>
             <input
               className="text-sm px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 "
+              value={assignTo}
+              onChange={(e) => {
+                setAssignTo(e.target.value);
+              }}
               type="text"
               placeholder="employee name"
             />
@@ -32,6 +86,10 @@ const CreateTask = () => {
             <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
             <input
               className="text-sm px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 "
+              value={category}
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }}
               type="text"
               placeholder="Design, Development, Marketing, etc"
             />
@@ -42,6 +100,10 @@ const CreateTask = () => {
           <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
           <textarea
             className="w-full h-44 text-sm px-4 rounded outline-none bg-transparent border-[1px] border-gray-400"
+            value={taskDescription}
+            onChange={(e) => {
+              setTaskDescription(e.target.value);
+            }}
             name=""
             id=""
             cols="30"
