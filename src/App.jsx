a343bf46d120d3007c2a8425ace3111b93ca697d
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-11 23:02:17 +0530

    Add updateTaskData function for task modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,55 +2,78 @@ import React, { useContext, useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
-import { admin, employees, getLocalStorage, setLocalStorage } from "./utils/localStorage";
 import { AuthContext } from "./context/AuthProvider";
 
 function App() {
-  
   const [user, setUser] = useState(null);
   const [loggedInUserData, setLoggedInUserData] = useState(null);
 
   // context
-  const authData = useContext(AuthContext);
-  // console.log(authData)
+  const { userData: authData } = useContext(AuthContext);
 
   useEffect(() => {
-    const loggedInUser = localStorage.getItem("loggedInUser")
+    const loggedInUser = localStorage.getItem("loggedInUser");
 
     if (loggedInUser) {
       const userData = JSON.parse(loggedInUser);
       setUser(userData.role);
       setLoggedInUserData(userData.data);
     }
-  }, [])
+  }, []);
+
+  const updateTaskData = (updatedTaskData) => {
+    setLoggedInUserData((prevData) => ({
+      ...prevData,
+      tasks: updatedTaskData, // Update tasks when modified
+    }));
+  };
 
   const handleLogin = (email, password) => {
     if (authData) {
       // Check if the user is an admin
-      const admin = authData.adminData.find((e) => email == e.email && e.password == password);
+      const admin = authData.adminData?.find(
+        (e) => email === e.email && e.password === password
+      );
       if (admin) {
         setUser("admin");
-        localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin", data: admin }));
+        localStorage.setItem(
+          "loggedInUser",
+          JSON.stringify({ role: "admin", data: admin })
+        );
         return;
       }
 
       // Check if the user is an employee
-      const employee = authData.employeesData.find((e) => email === e.email && e.password === password);
+      const employee = authData.employeesData?.find(
+        (e) => email === e.email && e.password === password
+      );
       if (employee) {
         setUser("employee");
         setLoggedInUserData(employee);
-        localStorage.setItem("loggedInUser", JSON.stringify({ role: "employee", data: employee }));
-        return
+        localStorage.setItem(
+          "loggedInUser",
+          JSON.stringify({ role: "employee", data: employee })
+        );
+        return;
       }
     }
 
     alert("Invalid credentials");
-  }
+  };
 
   return (
     <>
-      {!user ? <Login handleLogin={handleLogin} /> : ""}
-      {user == "admin" ? <AdminDashboard changeUser={setUser} /> : (user == "employee" ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null)  }
+      {!user ? (
+        <Login handleLogin={handleLogin} />
+      ) : user === "admin" ? (
+        <AdminDashboard changeUser={setUser} />
+      ) : user === "employee" ? (
+        <EmployeeDashboard
+          changeUser={setUser}
+          data={loggedInUserData}
+          updateTaskData={updateTaskData} // Pass function to update task data
+        />
+      ) : null}
     </>
   );
 }
