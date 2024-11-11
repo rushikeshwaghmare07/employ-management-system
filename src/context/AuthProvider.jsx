eeb7d32c40157ad85308f4a6c0087cbfaa9254bf
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-11-11 23:08:17 +0530

    Update auth provider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,24 +1,29 @@
 import React, { createContext, useEffect, useState } from "react";
 import { getLocalStorage, setLocalStorage } from "../utils/localStorage";
 
+// Create a context for authentication data
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState({
+    employeesData: [],
+    adminData: [],
+  });
 
   useEffect(() => {
-    // localStorage.clear()
-    setLocalStorage()
     const { employeesData, adminData } = getLocalStorage();
+
+    if (!employeesData || !adminData) {
+      setLocalStorage();
+    }
+
     setUserData({ employeesData, adminData });
   }, []);
 
   return (
-    <div>
-      <AuthContext.Provider value={userData}>
-        {children}
-      </AuthContext.Provider>
-    </div>
+    <AuthContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
